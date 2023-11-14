34c46de20ba97a625c7fccdfe518d840c8e2096f
DEV-Shaozi <shalomfaithfulness01@gmail.com>
2023-11-14 08:50:31 -0800

    printf

--- main.h
+++ main.h
@@ -21,6 +21,18 @@
 #define S_SHORT 1
 
 /**
+ * struct fmt - Struct op
+ *
+ * @fmt: The format.
+ * @fn: The function associated.
+ */
+struct fmt
+{
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
+
+/**
  * struct format - converter for printf
  * @ph: type char pointer of the specifier
  * @function: function for the conversion specifier
@@ -69,5 +81,19 @@ int is_digit(char);
 
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
+
+int handle_write_char(char c, char buffer[],
+	int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[],
+	int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision,
+	int length, char padd, char extra_c);
+int write_pointer(char buffer[], int ind, int length,
+	int width, int flags, char padd, char extra_c, int padd_start);
+
+int write_unsgnd(int is_negative, int ind, char buffer[],
+	int flags, int width, int precision, int size);
+int print_non_printable(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 
 #endif
